8728ae2ef18ce5fd8063be787f6c656402979c67
Serega <Schuganiserega@mail.ru>
2021-07-14 17:05:08 +0400

    Add project files.

---
+++ CresticiNolici_CSharp/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CresticiNolici_CSharp
+{
+    public class Game
+    {
+        public Message Info;
+        public Message Error;
+
+        public string Player1_Name;
+        public string Player2_Name;
+
+        public void Play()
+        {
+                Info?.Invoke("Противостояние игроков " + Player1_Name +" против "+ Player2_Name + "начинается");
+                if (Console.ReadKey().Key != ConsoleKey.Escape)
+                {
+                    Console.WriteLine("\n\n\n\n\n\n\n\n");
+                    for (int i = 6; i >= 0; i -= 3)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (j != 2)
+                            {
+                                Console.WriteLine("   |   |   ");
+                            }
+                        }
+                        if (i != 0) Console.WriteLine("--- --- ---");
+                    }
+                }
+                Console.ReadLine();
+        }
+
+		public void Exit()
+        {
+            Console.WriteLine("До новых встреч!");
+            return;
+        }
+    }
+}
---
+++ CresticiNolici_CSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CresticiNolici_CSharp
+{
+	public class Program
+	{
+        static void Main()
+        {
+            Console.WriteLine("Начнём?");
+
+            Game game = new Game();
+            game.Info = PlayInfo;
+            game.Error = PlayError;
+
+            string Player1_Name = "";
+            string Player2_Name = "";
+
+            for (; ; )
+            {
+                int cislo = 0;
+
+                Console.WriteLine("1. Играть", cislo);
+                Console.WriteLine("2. Выйти", cislo);
+                Console.WriteLine("Игрок 1 введите имя - ", Player1_Name);
+                Console.WriteLine("Игрок 1 введите имя - ", Player2_Name);
+                if (cislo == 1) game.Play();
+                if (cislo == 2) game.Exit();
+            }
+
+            static void PlayInfo(string message)
+            {
+                Console.WriteLine(message);
+            }
+            static void PlayError(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
+		
+		
